37ab0f52ba673e103ca820bdeda29b40f30c309e
guolongkang <1614696693@qq.com>
2019-12-11 21:42:09 +0800

    更新看看

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
+/*求质数*/
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
-        int a = 2;
-        String aa = Integer.toBinaryString(a);
-        System.out.println(aa);
-        binaryToDecimal(a);
+        int a = cin.nextInt();
+        int i;
+        for(i=2;i<a;i++){
+            if(a%i==0) break;
+        }
+        if(a == 1) System.out.println("Y");
+        else if(i==a) System.out.println("Y");
+        else System.out.println("N");
         cin.close();
-    }
-    private static void binaryToDecimal(int n){
-        for(int i = 31;i >= 0; i--)
-            System.out.print(n >>> i & 1);
     }
 }
