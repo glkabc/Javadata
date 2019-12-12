b7816e9803e52afe65019535f1c4a87aae70ba84
guolongkang <1614696693@qq.com>
2019-12-12 18:44:04 +0800

    能被3，5整除的数

---
+++ today_0/src/P1201.java
@@ -0,0 +1,21 @@
+/*
+*  by:today
+*  time:2019.12.12
+*  name:ACM 1201 被3和5整除的数
+* */
+import java.util.Scanner;
+
+public class P1201 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int m = cin.nextInt();
+        int n = cin.nextInt();
+        int num_sum = 0;
+        for(int i=m;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                num_sum++;
+            }
+        }
+        System.out.println(num_sum);
+    }
+}
