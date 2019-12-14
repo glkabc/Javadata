1b6fc8b5ff719eda7564650ebe8d9e6e0581451b
guolongkang <1614696693@qq.com>
2019-12-14 19:24:07 +0800

    按要求输出信息

---
+++ today_0/src/P2211.java
@@ -0,0 +1,18 @@
+/*
+*  name:today
+*  time:2019.12.14
+*  ACM:按要求输出信息
+*
+* */
+
+import java.util.Scanner;
+
+public class P2211 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.println("********************");
+        System.out.println("Very Good!");
+        System.out.println("******************** ");
+        cin.close();
+    }
+}
